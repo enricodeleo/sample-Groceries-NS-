474aac15f793661db29d2594f4f5ddd5613620d6
Enrico Deleo <enricodeleo@gmail.com>
2016-11-26 17:36:46 +0100

    retrieve grocerylist from apis

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Grocery } from '../../shared/grocery/grocery';
+import { GroceryListService } from '../../shared/grocery/grocery-list.service';
 
 @Component({
   selector: 'list',
   templateUrl: 'pages/list/list.html',
-  styleUrls: ['pages/list/list-common.css', 'pages/list/list.css']
+  styleUrls: [
+    'pages/list/list-common.css',
+    'pages/list/list.css'
+  ],
+  providers: [ GroceryListService ]
 })
 
-export class ListComponent {}
+export class ListComponent implements OnInit {
+  groceryList: Array<Grocery> = [];
+
+  constructor( private groceryListService: GroceryListService ) {}
+
+  ngOnInit() {
+    this.groceryListService.load()
+      .subscribe(loadedGroceries => {
+        loadedGroceries.forEach((groceryObject) => {
+          this.groceryList.unshift(groceryObject);
+        });
+      });
+  }
+}
