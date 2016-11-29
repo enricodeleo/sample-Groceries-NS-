3d293752ce50f6269102a0db23636de246c65beb
Enrico Deleo <enricodeleo@gmail.com>
2016-11-29 23:33:30 +0100

    include slideshow on login

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,10 @@
 import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
 import { AppModule } from './app.module';
 import { setStatusBarColors } from './utils/status-bar-util';
+import {registerElement} from "nativescript-angular/element-registry";
+
+registerElement("Carousel", () => require("nativescript-carousel").Carousel);
+registerElement("CarouselItem", () => require("nativescript-carousel").CarouselItem);
 
 setStatusBarColors(); // Set statusbar color as soon as possible (namely before bootstrapping the app)
 
